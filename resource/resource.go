99deb3eb8a60046ae8d34eac2d1fe91bb394294e
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-10-18 12:38:48 +0100

    resource: add mutex

--- resource/resource.go
+++ resource/resource.go
@@ -3,6 +3,7 @@ package resource
 
 import (
 	"errors"
+	"sync"
 	"time"
 
 	avl "github.com/timdrysdale/interval/internal/trees/avltree"
@@ -13,10 +14,12 @@ import (
 
 // Resource represents the bookings of a resources
 type Resource struct {
-	bookings *avl.Tree
+	*sync.RWMutex `json:"-"`
+	bookings      *avl.Tree
 }
 
-// Booking represents a booking
+// Booking represents a booking. This is not used internally, it's just for
+// returning booking info from GetBookings()
 type Booking struct {
 	When interval.Interval
 	ID   uuid.UUID
@@ -25,12 +28,16 @@ type Booking struct {
 // New creates a new resource with no bookings
 func New() *Resource {
 	return &Resource{
-		bookings: avl.NewWith(interval.Comparator),
+		&sync.RWMutex{},
+		avl.NewWith(interval.Comparator),
 	}
 }
 
 // Delete removes a booking, if it exists
 func (r *Resource) Delete(delete uuid.UUID) error {
+
+	r.Lock()
+	defer r.Unlock()
 
 	slots := r.bookings.Keys() //these are given in order
 	IDs := r.bookings.Values()
@@ -48,6 +55,8 @@ func (r *Resource) Delete(delete uuid.UUID) error {
 
 // Request returns a booking, if it can be made
 func (r *Resource) Request(when interval.Interval) (uuid.UUID, error) {
+	r.Lock()
+	defer r.Unlock()
 
 	u := uuid.New()
 
@@ -64,12 +73,15 @@ func (r *Resource) Request(when interval.Interval) (uuid.UUID, error) {
 
 // GetCount returns the number of live bookings
 func (r *Resource) GetCount() int {
+	r.RLock()
+	defer r.RUnlock()
 	return r.bookings.Size()
 }
 
 // GetBookings returns all bookings
 func (r *Resource) GetBookings() ([]Booking, error) {
-
+	r.RLock()
+	defer r.RUnlock()
 	b := []Booking{}
 
 	slots := r.bookings.Keys() //these are given in order
@@ -92,7 +104,8 @@ func (r *Resource) GetBookings() ([]Booking, error) {
 
 // ClearBefore removes all old bookings
 func (r *Resource) ClearBefore(t time.Time) {
-
+	r.Lock()
+	defer r.Unlock()
 	slots := r.bookings.Keys() //these are given in order
 
 	for _, when := range slots {
