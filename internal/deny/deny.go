6cb0c08b3d81d3b0276fe260d194a39d54372689
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-01-19 23:34:12 +0000

    deny (fix): New() and URL handling

--- internal/deny/deny.go
+++ internal/deny/deny.go
@@ -2,14 +2,16 @@ package deny
 
 import (
 	"context"
+	"net/url"
+	"strings"
 	"sync"
 	"time"
 
-	"github.com/timdrysdale/interval/internal/login"
-
 	httptransport "github.com/go-openapi/runtime/client"
+	log "github.com/sirupsen/logrus"
 	ac "github.com/timdrysdale/interval/internal/ac/client"
 	ao "github.com/timdrysdale/interval/internal/ac/client/operations"
+	"github.com/timdrysdale/interval/internal/login"
 )
 
 type Request struct {
@@ -23,20 +25,18 @@ type Client struct {
 	*sync.Mutex
 	now     func() time.Time
 	Request chan Request
-	Scheme  string
 	Secret  string
 	Timeout time.Duration
 }
 
-func New(secret string, timeout time.Duration) *Client {
+func New() *Client {
 
 	return &Client{
 		&sync.Mutex{},
 		func() time.Time { return time.Now() },
 		make(chan Request, 64),
-		"https",
-		secret,
-		timeout,
+		"replaceme",
+		time.Minute,
 	}
 }
 
@@ -47,12 +47,48 @@ func (c *Client) SetNow(now func() time.Time) *Client {
 	return c
 }
 
+func (c *Client) SetSecret(secret string) *Client {
+	c.Lock()
+	defer c.Unlock()
+	c.Secret = secret
+	return c
+}
+
+func (c *Client) SetTimeout(d time.Duration) *Client {
+	c.Lock()
+	defer c.Unlock()
+	c.Timeout = d
+	return c
+}
+
+func (c *Client) WithNow(now func() time.Time) *Client {
+	c.Lock()
+	defer c.Unlock()
+	c.now = now
+	return c
+}
+
+func (c *Client) WithSecret(secret string) *Client {
+	c.Lock()
+	defer c.Unlock()
+	c.Secret = secret
+	return c
+}
+
+func (c *Client) WithTimeout(d time.Duration) *Client {
+	c.Lock()
+	defer c.Unlock()
+	c.Timeout = d
+	return c
+}
+
+/*
 func (c *Client) WithScheme(scheme string) *Client {
 	c.Lock()
 	defer c.Unlock()
 	c.Scheme = scheme
 	return c
-}
+}*/
 
 func (c *Client) Run(ctx context.Context) {
 
@@ -85,17 +121,26 @@ func (c *Client) Run(ctx context.Context) {
 			stoken, err := login.Sign(token, c.Secret)
 
 			if err != nil { //token should generate ok, unless secret is blank?
-				req.Result <- "signing admin token failed because " + err.Error()
+				req.Result <- "signing admin token failed because" + err.Error()
 			}
 
 			auth := httptransport.APIKeyAuth("Authorization", "header", stoken)
-			trans := ac.DefaultTransportConfig().WithHost(req.URL).WithSchemes([]string{c.Scheme})
+			URL, err := url.Parse(req.URL)
+			if err != nil {
+				req.Result <- "relay deny request failed because url parsing error" + err.Error()
+			}
+
+			host := strings.TrimPrefix(req.URL, URL.Scheme+"://")
+
+			log.Debugf("scheme: %s, host: %s", URL.Scheme, host)
+
+			trans := ac.DefaultTransportConfig().WithHost(host).WithSchemes([]string{URL.Scheme})
 			client := ac.NewHTTPClientWithConfig(nil, trans)
 			param := ao.NewDenyParams().WithTimeout(c.Timeout).WithBid(req.BookingID).WithExp(req.ExpiresAt)
 			payload, err := client.Operations.Deny(param, auth)
 
 			if err != nil {
-				req.Result <- "relay deny request failed because " + err.Error()
+				req.Result <- "relay deny request failed because" + err.Error()
 			}
 
 			if !payload.IsSuccess() {
--- internal/deny/deny_test.go
+++ internal/deny/deny_test.go
@@ -1,22 +1,43 @@
 package deny
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"net/http"
 	"net/http/httptest"
-	"strings"
+	"os"
 	"testing"
-	"time"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
+
+var debug bool
+
+func init() {
+	debug = true
+	if debug {
+		log.SetReportCaller(true)
+		log.SetLevel(log.DebugLevel)
+		log.SetFormatter(&log.TextFormatter{FullTimestamp: false, DisableColors: true})
+		defer log.SetOutput(os.Stdout)
+
+	} else {
+		log.SetLevel(log.WarnLevel)
+		var ignore bytes.Buffer
+		logignore := bufio.NewWriter(&ignore)
+		log.SetOutput(logignore)
+	}
+
+}
 
 func TestDeny(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	client := New("some_uuid", time.Second).WithScheme("http")
+	client := New()
 
 	go client.Run(ctx)
 
@@ -29,7 +50,7 @@ func TestDeny(t *testing.T) {
 	r := make(chan string)
 
 	client.Request <- Request{
-		URL:       strings.TrimPrefix(server.URL, "http://"),
+		URL:       server.URL,
 		BookingID: "bid0",
 		ExpiresAt: 1674164170,
 		Result:    r,
