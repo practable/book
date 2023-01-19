0166ba05d2b3fbbe79851dd823f5494ad3b4c88c
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-01-19 23:49:19 +0000

    ac (fix): re-add patch for error printing

--- scripts/generate_access_client.sh
+++ scripts/generate_access_client.sh
@@ -2,4 +2,6 @@
 rm -rf ../internal/ac/models
 rm -rf ../internal/ac/restapi
 swagger generate client -t ../internal/ac -f ../api/access.yml -A ac
+# add in patches
+cp ../patch/internal/ac/models/pretty.go ../internal/ac/models/
 go mod tidy
